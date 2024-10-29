6077e082f6492f8ff31a54cb0c192060fac1457e
Raissa Alves Navarro <50011233800>
2024-10-29 11:25:29 -0300

    29/10/2024 v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import estilos from'./App.module.css'
-import { Cabecalho } from './Componentes/Cabecalho';
-import { BarraNavegacao } from './Componentes/BarradeNavegacao'
-import { Conteudo } from './Componentes/Conteudo'
-
-
+import { Home } from "./Paginas/Home"
 
 function App() {
   return (
     <>
-    <Cabecalho/>
-    <BarraNavegacao/>
-    <Conteudo/>
+    <Home/>
    </>
   )
 }
--- src/Componentes/Cabecalho.jsx
+++ src/Componentes/Cabecalho.jsx
@@ -3,7 +3,7 @@ import estilos from './Cabecalho.module.css';
 export function Cabecalho(){
     return(
     <header>
-        <h1>LariFlix</h1>
+        <h1>Zizaflix</h1>
     </header>
     )
 }
